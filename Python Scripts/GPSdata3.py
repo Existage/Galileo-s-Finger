6b591e290e773885c1cbf0a31844eb06357cb167
Travis Barrett <1andonlytravis@gmail.com>
2019-04-06 13:29:29 +0200

    python 3 version availible

--- Python Scripts/GPSdata3.py
+++ Python Scripts/GPSdata3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  gpsdata.py
+#  GPSdata3.py
 #  
-#  Copyright 2019 Travis <travis@travis-Dell-System-XPS-L502X>
+#  Copyright 2019 Travis Barrett
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -21,27 +21,42 @@
 #  MA 02110-1301, USA.
 #  
 #  
+#imports:
 import subprocess
+import datetime
+
+class Location:
+	def __init__(self, lat, lgt, alt, time):
+		self.lat = lat
+		self.lgt = lgt
+		self.alt = alt
+		self.time = time
+		
+
+
+
 
 def getRaw(): #returns raw gpspipe data as string
-	p = subprocess.Popen("gpspipe -r -n 50", stdout=subprocess.PIPE , shell=True)#opens pipe to gpspipe for 5 packets of data
+	p = subprocess.Popen("gpspipe -r -n 50", stdout=subprocess.PIPE , shell=True)#opens pipe to gpspipe for 50 packets of data
 	(output, err) = p.communicate()
 	p_status = p.wait()
-	#print "GPS NMEA DATA: ", output
+	#print( "GPS NMEA DATA: ", output)
 	#print "ERROR MESSAGES: ", err
 	return output
 
 def findFixes(data): #finds GPGGA sentences and returns list of entries
 	term = "GPGGA"
-	rawSentences = data.split("$")
+	info = str(data)
+	rawSentences = info.split('$')
+	#print(rawSentences)
 	sentences = []
 	for x in rawSentences: #loops through list and finds GPGGA sentences and removes \n char from entries
 		if term in x:
-			s = x.replace('\r\n' , '')
+			s = x.replace('\\r\\n' , '')
 			sentences.append(s)
 	
 	return sentences
-
+	
 def sentenceToList(sentence):#splits up sentence into list
 		out = sentence.split(',')
 		return out
@@ -63,7 +78,7 @@ def getLogitude(sentence): #fetches longitude from sentence and adds + or - to i
 		return('-',sentence[4])
 
 def getAlt(sentence):
-	return sentence[10]
+	return sentence[9]
 
 def average(data):
 	total = 0
@@ -77,32 +92,27 @@ def average(data):
 		return '+' + str(out)
 	else:
 		return str(out)
-	
-
-
 
 def main(args):
 	
-	
-	#data management
 	raw = getRaw()
 	fixList = findFixes(raw)
-	#print fixList
+	#print(fixList)
 	time = []
 	lat = []
 	lgt = []
+	alt = []
 	for i in fixList: #populate time, lat and lgt lists
 		line = sentenceToList(i)
 		time.append(getTime(line))
 		lat.append(getLatitude(line))
 		lgt.append(getLogitude(line))
+		alt.append(getAlt(line))
+		
 	
 	
-	#print (average(lat),average(lgt),int(time[-1])) 	
-	print ('--latitude {}d{}\"{}\' --longitude {}d{}\"{}\' --time {}'.format(average(lat)[0:3],average(lat)[3:5],average(lat)[6:8]+'.'+average(lat)[8:], average(lgt)[0:3],average(lgt)[3:5],average(lgt)[6:8]+'.'+average(lgt)[8:], time[-1]))
-	
-	
-	#sortedList
+		
+	print ('--latitude {}d{}\'{}" --longitude {}d{}\'{}" --sky-time {}:{}:{} --altitude {}'.format(average(lat)[0:3],average(lat)[3:5],average(lat)[6:8]+'.'+average(lat)[8:], average(lgt)[0:3],average(lgt)[3:5],average(lgt)[6:8]+'.'+average(lgt)[8:], time[-1][0:2],time[-1][2:4],time[-1][4:], round(float(average(alt)), 1)))	
 	
 	return 0
 
